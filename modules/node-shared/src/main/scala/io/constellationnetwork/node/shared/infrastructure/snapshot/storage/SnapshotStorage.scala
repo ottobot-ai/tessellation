74caf6752ca40c16a8dd9406b6e99412e8fbf550
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2026-03-10 20:45:19 -0300

    fix: fixing L1 sync issues and consensus spam logs
    (cherry picked from commit ff2c373df6917b31535ed84dd7b93ebccef35daa)

--- modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/infrastructure/snapshot/storage/SnapshotStorage.scala
+++ modules/node-shared/src/main/scala/io/constellationnetwork/node/shared/infrastructure/snapshot/storage/SnapshotStorage.scala
@@ -164,12 +164,18 @@ object SnapshotStorage {
                 notPersistedCache.update(current => current + snapshot.ordinal)
             )
           } >>
-          snapshotInfoLocalFileSystemStorage.write(snapshot.ordinal, snapshotInfo) >>
-          snapshotInfoCutoffQueue.offer(snapshot.ordinal) >>
-          snapshot.ordinal
-            .partialPreviousN(inMemoryCapacity)
-            .fold(Applicative[F].unit)(offloadQueue.offer) >>
-          combinedSnapshotCheckpointFileSystemStorage.tryWrite(snapshot.ordinal, snapshot, snapshotInfo, hash)
+          snapshotInfoLocalFileSystemStorage
+            .write(snapshot.ordinal, snapshotInfo)
+            .flatMap { _ =>
+              snapshotInfoCutoffQueue.offer(snapshot.ordinal) >>
+                snapshot.ordinal
+                  .partialPreviousN(inMemoryCapacity)
+                  .fold(Applicative[F].unit)(offloadQueue.offer) >>
+                combinedSnapshotCheckpointFileSystemStorage.tryWrite(snapshot.ordinal, snapshot, snapshotInfo, hash)
+            }
+            .handleErrorWith { e =>
+              logger.error(e)(s"Failed writing snapshot info to disk! ordinal=${snapshot.ordinal}. Skipping cutoff and checkpoint.")
+            }
       }
 
     def snapshotExists(snapshot: Signed[S])(implicit hasher: Hasher[F]): F[Boolean] =
