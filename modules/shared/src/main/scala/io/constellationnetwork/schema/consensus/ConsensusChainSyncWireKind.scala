eb6b6672554c4a3178f764bd2668011842b71459
OttoBot <ottobot@kd5ujc.xyz>
2026-07-16 17:55:20 -0500

    style(shared): apply scalafmt

--- modules/shared/src/main/scala/io/constellationnetwork/schema/consensus/ConsensusChainSyncWireKind.scala
+++ modules/shared/src/main/scala/io/constellationnetwork/schema/consensus/ConsensusChainSyncWireKind.scala
@@ -14,20 +14,16 @@ sealed abstract class ConsensusChainSyncWireKind(
 object ConsensusChainSyncWireKind {
   import ConsensusCarrierKind._
 
-  case object ChainPointComponent
-      extends ConsensusChainSyncWireKind("transport.chainsync.wire.chain-point", ChainSyncSelectionHint)
+  case object ChainPointComponent extends ConsensusChainSyncWireKind("transport.chainsync.wire.chain-point", ChainSyncSelectionHint)
   case object FetchSnapshotsRequest
       extends ConsensusChainSyncWireKind("transport.chainsync.wire.fetch-snapshots-request", ChainSyncSnapshotCarrier)
-  case object SnapshotResponse
-      extends ConsensusChainSyncWireKind("transport.chainsync.wire.snapshot-response", ChainSyncSnapshotCarrier)
+  case object SnapshotResponse extends ConsensusChainSyncWireKind("transport.chainsync.wire.snapshot-response", ChainSyncSnapshotCarrier)
   case object FindIntersectionRequest
       extends ConsensusChainSyncWireKind("transport.chainsync.wire.find-intersection-request", ChainSyncSelectionHint)
   case object FindIntersectionResponse
       extends ConsensusChainSyncWireKind("transport.chainsync.wire.find-intersection-response", ChainSyncSelectionHint)
-  case object GetPeerTipRequest
-      extends ConsensusChainSyncWireKind("transport.chainsync.wire.get-peer-tip-request", ChainSyncSelectionHint)
-  case object PeerTipResponse
-      extends ConsensusChainSyncWireKind("transport.chainsync.wire.peer-tip-response", ChainSyncSelectionHint)
+  case object GetPeerTipRequest extends ConsensusChainSyncWireKind("transport.chainsync.wire.get-peer-tip-request", ChainSyncSelectionHint)
+  case object PeerTipResponse extends ConsensusChainSyncWireKind("transport.chainsync.wire.peer-tip-response", ChainSyncSelectionHint)
   case object ServeSnapshotsRequest
       extends ConsensusChainSyncWireKind("transport.chainsync.wire.serve-snapshots-request", ChainSyncSnapshotCarrier)
   case object ServeChainPointsRequest
